515c5297def2157bc073cfe9cbc935f12fb11367
sergui20 <38196193+sergui20@users.noreply.github.com>
2019-03-11 14:21:54 -0500

    Update app.js

--- src/pages/containers/app.js
+++ src/pages/containers/app.js
@@ -1,16 +1,15 @@
-import React from 'react'; // React sirve para crear componentes
+import React from 'react';
 import Videos from './videos';
 import Home from '../components/home';
 import NotFound from '../components/not-found';
 import Header from '../components/header';
 import Video from './video';
 
-import { createStore, applyMiddleware } from 'redux'; // Para crear nuestro unico store de HOME
-import { Provider } from 'react-redux'; // Es un HOC, o un componente de alto orden que pasara los datos del store a los componentes hijos.
-import reducer from '../../reducers/index'; // Requerimos nuestra funcion pura o tambien llamada reducer, es lo que cambiara el estado de nuestra aplicacion.
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from '../../reducers/index';
 
-import { Map as map } from 'immutable'; // Todos los objetos pasan a ser mapas
-
+import { Map as map } from 'immutable'; // Maps instead of objects
 //Middleware redux
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -21,40 +20,6 @@ import thunk from 'redux-thunk';
 //React-router
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-// // Crearemos un reducer para cada key del initialState
-// const initialState = { // Estado inicial de redux que le pasaremos a los componentes hijos
-//     data: {
-//         entities: data.entities,
-//         categories: data.result.categories,
-//         search: [] // Colocamos un array para la busqueda, aqui se guardaran todas las busquedas que hagamos.
-//     },
-//     modal: {
-//         visibility: false,
-//         mediaId: null
-//     }
-// }
-
-// function logger ({dispatch, getState}) {
-//     return (next) => {
-//         return (action) => {
-//             console.log(`Accion: ${action}`)
-//             const value = next(action)
-//             console.log(`Nuevo estado: ${getState().toJS()}`)
-//             return value
-//         }
-//     }
-// }
-
-// const logger = ({dispatch, getState}) => next => action => {
-//     console.log(`Accion: ${JSON.stringify(action)}`)
-//     debugger;
-//     const value = next(action);
-//     debugger;
-//     console.log(`Nuevo estado: ${JSON.stringify(getState().toJS())}`);
-//     debugger;
-//     return value
-// }
-
 const store = createStore(
     reducer,
     map(),
@@ -63,12 +28,6 @@ const store = createStore(
     )
     // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Herramientas de desarrollo
 );
-
-// console.log(store.getState()); // getState() es para obtener el estado de nuestro store
-
-// ReactDOM.render(que voy a renderizar, donde lo hare)
-// Alt + 0191 es para signo de interrogacion, 164,165 para la Ñ, 130 para la é, 160 - 163 para las de mas vocales
-// Se puede utilizar ReactDOM.render o simplemente con el metodo render
 
 class App extends React.Component {
     render () {
