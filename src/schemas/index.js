3bb3bbeea3c6ef4634f86083f12f101a3d265e4f
sergui20 <38196193+sergui20@users.noreply.github.com>
2019-03-11 14:34:47 -0500

    Update index.js

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -3,15 +3,15 @@ import api from '../api.json';
 import {normalize, schema} from 'normalizr';
 
 const media = new schema.Entity('media', {}, {
-    idAttribute: 'id', // Para que tome el id que viene por defecto en nuestra data.json. Puede ser tambien una funcion que recibe tres parametros: value, parent, key
-    processStrategy: (value, parent, key) => ({...value, category: parent.id}) // Estrategia a utilizar antes de procesar mi entidad. Puedo agregar o modificar ciertos atributos
-}); // (key, definicion de mi esquema, opciones) En definicion de esquema es para heredar esquemas dentro de otros. Las opciones son para personalizar mi esquema
+    idAttribute: 'id',
+    processStrategy: (value, parent, key) => ({...value, category: parent.id})
+}); // (key, definicion de mi esquema, opciones)
 
-const category = new schema.Entity('categories', { // Aqui si agrego una definicion (2do parametro) para heredar al esquema anterior media.
-    playlist: new schema.Array(media) // Dentro de mis categorias hay playlists, entonces por eso heredamos a media porque ya lo definimos anteriormente. Media vendria a ser un playlist solo que lo hicimos con diferentes nombres. Ademas es un array, por eso definimos como schema.Array()
+const category = new schema.Entity('categories', {
+    playlist: new schema.Array(media)
 });
 
-const categories = { categories: new schema.Array(category)} // Categories sera lo que va a contener todo, por eso se lo declara al ultimo. Y simplemente hacemos un array con el esquema que definimos anteriormente
+const categories = { categories: new schema.Array(category)}
 
 const normalizeData = normalize(api, categories);
 
